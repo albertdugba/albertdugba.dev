49e41a5c62b1a2c6d7847b704ba74c166ae04a5d
Albert Dugba <albert.dugba@gmail.com>
2021-02-07 08:37:18 +0000

    added icons && 360 spin animation

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useRef } from 'react';
+import { FunctionComponent } from 'react';
 import { useCycle, motion } from 'framer-motion';
 import styled from 'styled-components';
 
--- pages/components/Hero/Hero.tsx
+++ pages/components/Hero/Hero.tsx
@@ -1,75 +1,127 @@
-import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import FireBase from '../../../styles/Icons/firebase';
+import Github from '../../../styles/Icons/github';
+import GraphQL from '../../../styles/Icons/graphql';
+import NextJS from '../../../styles/Icons/nextjs';
+import NodeJS from '../../../styles/Icons/node';
+import ReactJS from '../../../styles/Icons/react';
+import Redux from '../../../styles/Icons/redux';
+import Terminal from '../../../styles/Icons/terminal';
+import TypeScript from '../../../styles/Icons/typescript';
 
 const Hero = () => {
-  const [subIndex, setSubIndex] = useState(0);
-  const [reverse, setReverse] = useState(false);
-  const [index, setIndex] = useState(0);
-  const [blink, setBlink] = useState(true);
-
-  const words = ['Frontend Developer', 'Technical Writer', 'Problem Solver'];
-  // typeWriter
-  useEffect(() => {
-    if (index === words.length) return;
-
-    if (
-      subIndex === words[index].length + 1 &&
-      index !== words.length - 1 &&
-      !reverse
-    ) {
-      setReverse(true);
-      return;
-    }
-
-    if (subIndex === 0 && reverse) {
-      setReverse(false);
-      setIndex(prev => prev + 1);
-      return;
-    }
-
-    const timeout = setTimeout(() => {
-      setSubIndex(prev => prev + (reverse ? -1 : 1));
-    }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 : 150, Number(Math.random() * 350)));
-
-    return () => clearTimeout(timeout);
-  }, [subIndex, index, reverse]);
-
-  // blinker
-  useEffect(() => {
-    const timeout2 = setTimeout(() => {
-      setBlink(prev => !prev);
-    }, 400);
-    return () => clearTimeout(timeout2);
-  }, [blink]);
-
   return (
     <>
       <Container>
         <aside className="aside">
-          <h2 className="contaier">
-            Hi, welcome to my site, my name is Albert and I'm a{' '}
-            {`${words[index].substring(0, subIndex)}${blink ? '|' : ''}`}
-          </h2>
+          <h1>
+            Hi, welcome to my site, my name is Albert and I'm Software Engineer
+            {/* with a focus on frontend development */}
+          </h1>
         </aside>
 
-        <section>
-          <h1>Image goes here</h1>
-        </section>
+        <Section
+          initial={{ rotate: 0 }}
+          animate={{ rotate: 360 }}
+          transition={{
+            duration: 20,
+            ease: 'linear',
+            repeat: Infinity,
+          }}>
+          <li className="github--icon">
+            <Github width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="github--icon">
+            <Github width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="nodejs--icon">
+            <NodeJS width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="nextjs--icon">
+            <NextJS width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="terminal--icon">
+            <Terminal width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="typescript--icon">
+            <TypeScript width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="reactjs--icon">
+            <ReactJS width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="graphql--icon">
+            <GraphQL width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="firebase--icon">
+            <FireBase width="2.2rem" height="2.2rem" />
+          </li>
+          <li className="redux--icon">
+            <Redux width="2.2rem" height="2.2rem" />
+          </li>
+        </Section>
       </Container>
     </>
   );
 };
 
+const Section = styled(motion.section)`
+  position: relative;
+  width: 18rem;
+  height: 18rem;
+  padding: 2.8em;
+  border-radius: 50%;
+  margin: 1.75em auto 0;
+
+  li {
+    display: block;
+    overflow: hidden;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 4em;
+    height: 4em;
+    margin: -2em;
+  }
+
+  .github--icon {
+    transform: rotate(0deg) translate(10rem) rotate(0deg);
+  }
+  .nodejs--icon {
+    transform: rotate(45deg) translate(10rem) rotate(-45deg);
+  }
+  .terminal--icon {
+    transform: rotate(90deg) translate(10rem) rotate(-90deg);
+  }
+  .nextjs--icon {
+    transform: rotate(135deg) translate(9em) rotate(-135deg);
+  }
+  .graphql--icon {
+    transform: rotate(180deg) translate(10rem) rotate(-180deg);
+  }
+  .typescript--icon {
+    transform: rotate(225deg) translate(10rem) rotate(-225deg);
+  }
+  .firebase--icon {
+    transform: rotate(270deg) translate(10rem) rotate(-270deg);
+  }
+
+  .redux--icon {
+    transform: rotate(315deg) translate(10rem) rotate(-315deg);
+  }
+`;
+
 const Container = styled.div`
-  height: calc(100vh - 60px);
+  height: calc(100vh - 50px);
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: 1fr 1fr;
   align-items: center;
   background: #34223a;
   color: white;
   text-align: center;
   justify-content: center;
   font-family: var(--fontFamily2);
+  overflow: hidden;
 
   h2 {
   }
@@ -82,7 +134,7 @@ const Container = styled.div`
   .aside {
     margin-top: -100px;
     h1 {
-      font-size: 3rem;
+      font-size: 2.5rem;
       @media (max-width: 600px) {
         margin-top: 70px;
         font-size: 2.5rem;
