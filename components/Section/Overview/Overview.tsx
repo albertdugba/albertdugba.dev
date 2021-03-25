2d21edd8f653dde0b3c328af7404c8b29463c523
Albert Dugba <albert.dugba@gmail.com>
2021-03-25 07:15:23 +0000

    underline texts

--- components/Section/Overview/Overview.tsx
+++ components/Section/Overview/Overview.tsx
@@ -1,12 +1,26 @@
 import { Container } from './styles';
+import Link from 'next/link';
 
 const Overview = () => {
   return (
     <>
       <Container className="container">
-        <h1>
-          I am not a designer but I do enjoy design work with the magic of CSS
-        </h1>
+        <div className="text">
+          <p>
+            I am a JavaScript and Frontend Engineer. I design with CSS and
+            develop quality, user friendly, and scalable products with {'  '}
+            <span className="span1">performance</span>,{' '}
+            <span className="span2">accessibility</span>{' '}
+            <span className="span3">progressive enhancement</span> in mind from
+            the ground up.
+            <br /> I enjoy building design systems for products & projects.
+          </p>
+          <Link href="/works">
+            <button>
+              <a>See my works</a>
+            </button>
+          </Link>
+        </div>
 
         <div className="shapes">
           <div className="div1" id="one"></div>
--- components/Section/Overview/styles.ts
+++ components/Section/Overview/styles.ts
@@ -9,11 +9,48 @@ export const Container = styled.div`
     grid-template-columns: 1fr;
   }
 
-  h1 {
-    color: black;
+  .text p {
+    .span1 {
+      border-bottom: 2px solid blue;
+    }
+    .span2 {
+      border-bottom: 2px solid green;
+    }
+
+    .span3 {
+      border-bottom: 2px solid red;
+      transform: translate(-20px);
+    }
   }
 
-  .shapes {
+  button {
+    background: none;
+    border: none;
+    outline: none;
+    display: block;
+    font-size: 1rem;
+    font-weight: bold;
+    position: relative;
+    min-width: 180px;
+    padding: 1rem 2rem;
+    cursor: pointer;
+    font-family: inherit;
+    transition: background 0.4s ease-out;
+    background-color: blue;
+
+    a {
+      color: #fff;
+      text-decoration: none;
+    }
+  }
+
+  p {
+    font-size: 1.2rem;
+    color: #232324;
+    line-height: 1.8;
+  }
+
+  .shaps {
     animation: rotate-all 2s 0 infinite linear alternate;
   }
 
