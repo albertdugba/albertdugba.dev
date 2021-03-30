e056bd34629df8a345574722e1694548fa095d42
Albert Dugba <albert.dugba@gmail.com>
2021-03-30 17:16:23 +0000

    add a blog card

--- components/Section/Projects/Projects.tsx
+++ components/Section/Projects/Projects.tsx
@@ -1,15 +1,48 @@
 import Link from 'next/link';
-import { ButtonLink } from './styles';
+import { Container, ButtonLink, CardList, Card } from './styles';
 
 const Projects = () => {
   return (
-    <div className="container">
-      <h1>I design and experiments with new technologies.</h1>
-      <ButtonLink>
-        <Link href="/works">
-          <a>See my work</a>
-        </Link>
-      </ButtonLink>
+    <div style={{ background: '#201c29' }}>
+      <div className="container">
+        <Container>
+          <div>
+            <h1>I design and experiments with new technologies.</h1>
+            <ButtonLink>
+              <Link href="/works">
+                <a>See my work</a>
+              </Link>
+            </ButtonLink>
+          </div>
+
+          <CardList>
+            <Card>
+              <header className="card-header">
+                <p>31st March, 2021</p>
+                <h2>Blog Card</h2>
+              </header>
+            </Card>
+            <Card>
+              <header className="card-header">
+                <p>31st March, 2021</p>
+                <h2>Blog Card</h2>
+              </header>
+            </Card>
+            <Card>
+              <header className="card-header">
+                <p>31st March, 2021</p>
+                <h2>Blog Card</h2>
+              </header>
+            </Card>
+            <Card>
+              <header className="card-header">
+                <p>31st March, 2021</p>
+                <h2>Blog Card</h2>
+              </header>
+            </Card>
+          </CardList>
+        </Container>
+      </div>
     </div>
   );
 };
--- components/Section/Projects/styles.ts
+++ components/Section/Projects/styles.ts
@@ -17,4 +17,65 @@ const ButtonLink = styled.div`
   }
 `;
 
-export { ButtonLink };
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  /* background: #17141d; */
+  color: #fff;
+  padding: 1rem;
+`;
+
+const Card = styled.article`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  height: 350px;
+  width: 390px;
+  min-width: 250px;
+  padding: 1.5rem;
+  border-radius: 1rem;
+  background-color: #17141d;
+  box-shadow: -1rem 0 3rem #000;
+  transition: 0.2s;
+
+  &:hover {
+    transform: translateY(-1rem);
+  }
+
+  &:hover ~ & {
+    transform: translateX(50px);
+  }
+
+  &::not(:first-child) {
+    margin-left: -30px;
+  }
+`;
+const CardList = styled.section`
+  display: flex;
+  padding: 3rem;
+  width: 60%;
+  overflow-x: scroll;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+    height: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #201c29;
+    border-radius: 10px;
+    box-shadow: inset 2px 2px 2px hsla(0, 0%, 100%, 0.25) inset -2px -2px 2px
+      rgba(0, 0, 0, 0.2);
+  }
+
+  &::-webkit-scrollbar-track {
+    background: linear-gradient(90deg, #201c29, #201c29);
+  }
+`;
+
+const CardAuthor = styled.div``;
+const AuthorAvatar = styled.a``;
+const AuthorName = styled.div``;
+const AuthorNamePrefix = styled.div``;
+const Tags = styled.div``;
+
+export { Container, ButtonLink, Card, CardList };
