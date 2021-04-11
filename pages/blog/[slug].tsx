8968533ed4aadf80c8839842cf8cffbb8aed3327
Albert Dugba <albert.dugba@gmail.com>
2021-04-11 10:18:04 +0000

    added documents

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,45 +1,45 @@
 import { GraphQLClient } from 'graphql-request';
 
-const graphcms = new GraphQLClient(
-  'https://api-us-west-2.graphcms.com/v2/ckn7dbcigby0f01xrec441zrb/master'
-);
+// const graphcms = new GraphQLClient(
+//   'https://api-us-west-2.graphcms.com/v2/ckn7dbcigby0f01xrec441zrb/master'
+// );
 
-export const getStaticProps = async ({ params }) => {
-  const { post } = await graphcms.request(
-    `
-    query PostPageQuery($slug:String!){
-        post(where: {slug:$slug}){
-            title,
-            body,
-            tags,
-        }
-    }`,
-    {
-      slug: params.slug,
-    }
-  );
+// export const getStaticProps = async ({ params }) => {
+//   const { post } = await graphcms.request(
+//     `
+//     query PostPageQuery($slug:String!){
+//         post(where: {slug:$slug}){
+//             title,
+//             body,
+//             tags,
+//         }
+//     }`,
+//     {
+//       slug: params.slug,
+//     }
+//   );
 
-  return {
-    props: {
-      post,
-    },
-  };
-};
+//   return {
+//     props: {
+//       post,
+//     },
+//   };
+// };
 
-export const getStaticPaths = async () => {
-  const { posts } = await graphcms.request(`{
-        posts {
-            slug
-            title
-        }
-  
-    }`);
+// export const getStaticPaths = async () => {
+//   const { posts } = await graphcms.request(`{
+//         posts {
+//             slug
+//             title
+//         }
 
-  return {
-    paths: posts.map(({ slug }) => ({ params: { slug } })),
-    fallback: false,
-  };
-};
+//     }`);
+
+//   return {
+//     paths: posts.map(({ slug }) => ({ params: { slug } })),
+//     fallback: false,
+//   };
+// };
 
 const BlogPage = ({ post }) => {
   return <h1 className="container">Blog Page:{post.title}</h1>;
