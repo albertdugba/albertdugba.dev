daff9142ebfcd2f57786c418bbecd2f615e97e61
Albert Dugba <albert.dugba@gmail.com>
2021-04-30 14:23:20 +0000

    added profile image from app

--- components/Section/Home/Home.tsx
+++ components/Section/Home/Home.tsx
@@ -32,7 +32,7 @@ export const Home = () => {
             </div>
 
             <img
-              src="https://instagram.facc1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/165471823_894836077733531_4585199131174438146_n.jpg?tp=1&_nc_ht=instagram.facc1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=RB8y058YcWkAX-PddBr&ccb=7-4&oh=90736025cab2b3f938d2ab284e5d01cc&oe=608AD651&_nc_sid=83d603"
+              src="./albertdugba.jpeg"
               alt="Albert Dugba"
               width="100%"
               height="auto"
