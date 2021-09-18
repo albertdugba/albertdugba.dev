10fbcff2d6908248bddb8473ebce078800956fe8
Albert Dugba <albert.dugba@gmail.com>
2021-09-18 14:30:52 +0000

    file import config

--- public/icons/i.tsx
+++ public/icons/i.tsx
@@ -2,16 +2,9 @@ import { FunctionComponent } from 'react';
 import { IconProps } from 'src/lib/interface';
 
 const I: FunctionComponent<IconProps> = ({ size, ...rest }) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox='0 0 564 330'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-    {...rest}
-  >
-    <rect x='59' y='79' width='41' height='60' fill='#69186A' />
-    <circle cx='79.5' cy='46.5' r='20.5' fill='#6E1F94' />
+  <svg width={size} height={size} viewBox="0 0 564 330" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
+    <rect x="59" y="79" width="41" height="60" fill="#69186A" />
+    <circle cx="79.5" cy="46.5" r="20.5" fill="#6E1F94" />
   </svg>
 );
 
