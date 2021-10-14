b80c643a8d437930c55dec4cdf3da1b497511e48
Albert Dugba <albert.dugba@gmail.com>
2021-10-14 21:15:09 +0000

    fixed typo;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import { Loader } from '@/common/loader/loader';
 
-const Landing = dynamic(() => import('@/landing/Landing'), { loading: () => <Loader /> });
+const Landing = dynamic(() => import('@/landing/Landing'), { loading: () => <Loader title="Loading" /> });
 
 export default function Home() {
   return <Landing />;
