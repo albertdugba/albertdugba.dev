9b79ba08c07dff47141af72863b32c0e3a605ada
Albert <albert.dugba@gmail.com>
2024-07-03 19:36:20 +0100

    add reading time

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import { PostBody } from "~/components/posts/post-body";
 import { Title } from "~/components/title";
 import { getPost } from "~/lib/posts";
+import readingTime from "reading-time";
 
 async function PostPage({
   params,
@@ -22,17 +23,14 @@ async function PostPage({
     <div className='min-h-screen w-full items-center justify-center lg:mt-24 md:py-16 py-14'>
       <div>
         <Title title={post.title} />
-        <div className='flex p-2 gap-2'>
+        <div className='flex flex-wrap p-2 gap-2'>
           <span>{post.date}</span>
           <ul className='flex gap-2 mb-6'>
-            {post?.tags?.map((tag, i) => (
-              <li
-                className='list-none text-sm border border-secondary px-2.5 py-0.5 bg-secondary text-primary rounded-lg'
-                key={i}
-              >
-                {tag}
-              </li>
-            ))}
+            --{" "}
+            {"-- "}{!!post?.tags && (
+              <li className='!list-none'>{post.tags.join(",   ")}</li>
+            )}
+            -- {readingTime(post.body).text}
           </ul>
         </div>
         <div className='relative shadow-2xl lg:h-[500px] h-[300px] my-8'>
--- src/components/posts/post-card.tsx
+++ src/components/posts/post-card.tsx
@@ -4,26 +4,12 @@ import { Post } from "~/types";
 
 interface BlogCard {
   post?: Post;
-  href?: string;
 }
-export const BlogCard = ({ post, href }: BlogCard) => {
+export const BlogCard = ({ post }: BlogCard) => {
   return (
     <div className='w-full rounded-xl text-white p-2 hover:border-2 hover:border-secondary  hover:transition-all hover:-translate-y-2'>
-      {href ? (
-        <Link href={href}>
-          <div className='relative lg:h-[200px] h-[180px] w-full mb-4'>
-            <Image
-              src='/blog/pnpm.avif'
-              placeholder='blur'
-              blurDataURL='/blog/pnpm.avif'
-              fill
-              alt=''
-              className='w-full h-auto object-cover rounded-xl'
-            />
-          </div>
-        </Link>
-      ) : (
-        <div className='relative w-full lg:h-[180px] h-[180px] mb-4'>
+      <Link href={`/blog/${post?.slug}`}>
+        <div className='relative lg:h-[200px] h-[180px] w-full mb-4'>
           <Image
             src='/blog/pnpm.avif'
             placeholder='blur'
@@ -33,17 +19,17 @@ export const BlogCard = ({ post, href }: BlogCard) => {
             className='w-full h-auto object-cover rounded-xl'
           />
         </div>
-      )}
+      </Link>
 
-      <div>
+      <Link href={`/blog/${post?.slug}`}>
         <h1 className='text-xl text-primary mb-3 text-white'>{post?.title}</h1>
 
         <div className='h-16 mb-5 w-full'>
-          <p className='text-sm font-light text-white line-clamp-4 p-1'>
+          <p className='text-sm font-light text-white line-clamp-4'>
             {post?.description}
           </p>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
