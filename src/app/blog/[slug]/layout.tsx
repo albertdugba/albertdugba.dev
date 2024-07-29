aaad314a5e7b7a643e0a1ec2c769ba7e420b3dd7
Albert <albert.dugba@gmail.com>
2024-07-29 14:23:18 +0100

    metadatabase

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -18,10 +18,8 @@ export const generateMetadata = async ({
 
   const previousImages = [post?.image];
 
-  console.log(previousImages);
-
   return {
-    metadataBase: new URL("https://albertdugba.dev/"),
+    metadataBase: new URL("https://albertdugba.dev"),
     title: post?.title,
     description: post?.description,
     keywords: post?.tags,
